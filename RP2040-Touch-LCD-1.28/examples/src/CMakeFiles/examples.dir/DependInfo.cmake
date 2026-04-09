8ece9d9336c09adc5862047b8dff64f12147eb19
Rubber Cat <rubbercat3@gmail.com>
2026-04-09 17:29:37 -0400

    WIP: Setting up RP2350 version...

---
+++ RP2040-Touch-LCD-1.28/examples/src/CMakeFiles/examples.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/ImageData.c" "CMakeFiles/examples.dir/ImageData.o" "gcc" "CMakeFiles/examples.dir/ImageData.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/LCD_1in28_LVGL_test.c" "CMakeFiles/examples.dir/LCD_1in28_LVGL_test.o" "gcc" "CMakeFiles/examples.dir/LCD_1in28_LVGL_test.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/LVGL_example.c" "CMakeFiles/examples.dir/LVGL_example.o" "gcc" "CMakeFiles/examples.dir/LVGL_example.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/RCatLogo.c" "CMakeFiles/examples.dir/RCatLogo.o" "gcc" "CMakeFiles/examples.dir/RCatLogo.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/black.c" "CMakeFiles/examples.dir/black.o" "gcc" "CMakeFiles/examples.dir/black.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/evileye.c" "CMakeFiles/examples.dir/evileye.o" "gcc" "CMakeFiles/examples.dir/evileye.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/hal9000.c" "CMakeFiles/examples.dir/hal9000.o" "gcc" "CMakeFiles/examples.dir/hal9000.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/home.c" "CMakeFiles/examples.dir/home.o" "gcc" "CMakeFiles/examples.dir/home.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/img_bulb_gif.c" "CMakeFiles/examples.dir/img_bulb_gif.o" "gcc" "CMakeFiles/examples.dir/img_bulb_gif.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/img_bulb_gif.tweaked.c" "CMakeFiles/examples.dir/img_bulb_gif.tweaked.o" "gcc" "CMakeFiles/examples.dir/img_bulb_gif.tweaked.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/nonbinary.c" "CMakeFiles/examples.dir/nonbinary.o" "gcc" "CMakeFiles/examples.dir/nonbinary.o.d"
+  "/home/greg/repo/rcat-MSA-LCD/examples/src/radar.c" "CMakeFiles/examples.dir/radar.o" "gcc" "CMakeFiles/examples.dir/radar.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RP2040-Touch-LCD-1.28/examples/src/CMakeFiles/examples.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/examples.dir/ImageData.o"
+  "CMakeFiles/examples.dir/ImageData.o.d"
+  "CMakeFiles/examples.dir/LCD_1in28_LVGL_test.o"
+  "CMakeFiles/examples.dir/LCD_1in28_LVGL_test.o.d"
+  "CMakeFiles/examples.dir/LVGL_example.o"
+  "CMakeFiles/examples.dir/LVGL_example.o.d"
+  "CMakeFiles/examples.dir/RCatLogo.o"
+  "CMakeFiles/examples.dir/RCatLogo.o.d"
+  "CMakeFiles/examples.dir/black.o"
+  "CMakeFiles/examples.dir/black.o.d"
+  "CMakeFiles/examples.dir/evileye.o"
+  "CMakeFiles/examples.dir/evileye.o.d"
+  "CMakeFiles/examples.dir/hal9000.o"
+  "CMakeFiles/examples.dir/hal9000.o.d"
+  "CMakeFiles/examples.dir/home.o"
+  "CMakeFiles/examples.dir/home.o.d"
+  "CMakeFiles/examples.dir/img_bulb_gif.o"
+  "CMakeFiles/examples.dir/img_bulb_gif.o.d"
+  "CMakeFiles/examples.dir/img_bulb_gif.tweaked.o"
+  "CMakeFiles/examples.dir/img_bulb_gif.tweaked.o.d"
+  "CMakeFiles/examples.dir/nonbinary.o"
+  "CMakeFiles/examples.dir/nonbinary.o.d"
+  "CMakeFiles/examples.dir/radar.o"
+  "CMakeFiles/examples.dir/radar.o.d"
+  "libexamples.a"
+  "libexamples.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/examples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
